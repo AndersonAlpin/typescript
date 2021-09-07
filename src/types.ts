3ba1edf7f2a19cba17cbfa52692a370574c4494a
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-09-07 12:15:14 -0300

    Add typescript types

---
+++ src/types.ts
@@ -0,0 +1,55 @@
+// boolean (true/false)
+let isOnline: boolean;
+isOnline = true;
+
+// string ('foo', "foo", `foo`)
+let msg: string;
+msg = "All ok"
+
+// number (int, float, hex, binary)
+let value: number;
+value = 0x313;
+
+// array (type[])
+let numbers: number[];
+numbers = [12, 15, 51, 55];
+
+let values: Array<number>;
+values = [1, 2, 3];
+
+// tuple
+let defined: [number, string, boolean]
+defined = [1, "position", true]
+
+// enum
+enum hexColors {
+  black = "#000",
+  white = "#fff"
+}
+
+// any (anything) NOT RECOMMENDED
+let anything: any;
+anything = "any";
+anything = [1];
+anything = 10;
+
+// void (empty)
+function clg(): void {
+  console.log('nothing')
+}
+
+// null/undefined
+type newType = number | undefined;
+
+// never
+function error(): never {
+  throw new Error("not implemented");
+}
+
+// object
+let person: object;
+person = {
+  firstName: "Dave",
+  lastName: "Grohl",
+  age: 30,
+}
