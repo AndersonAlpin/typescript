848acb5f0b4dba94fb2b2b5861fb452d77b3b2c7
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-09-12 00:18:13 -0300

    Subclassing and acess modifier

--- dist/classes.js
+++ dist/classes.js
@@ -1,13 +1,47 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var User = /** @class */ (function () {
     function User(name, age) {
         this.name = name;
         this.age = age;
     }
-    User.prototype.printDetails = function () {
-        console.log("Name: " + this.name + " Age: " + this.age);
+    User.prototype.printDetailsUser = function () {
+        console.log("Name: " + this.name + " - Age: " + this.age);
     };
     return User;
 }());
+var UserAccount = /** @class */ (function (_super) {
+    __extends(UserAccount, _super);
+    function UserAccount(name, age, nickname, level) {
+        var _this = _super.call(this, name, age) || this;
+        _this.nickname = nickname;
+        _this.level = level;
+        return _this;
+    }
+    UserAccount.prototype.printDetailsUserAccount = function () {
+        console.log("Nickname: " + this.nickname + " - Age: " + this.age + "  - Level: " + this.level);
+    };
+    return UserAccount;
+}(User));
 var firstUser = new User("Anderson", 27);
-firstUser.printDetails();
+firstUser.printDetailsUser();
+var secondUser = new UserAccount("Anderson", 27, "Alpin", 53);
+// secondUser.name = "Bruno"
+console.log(secondUser);
+console.log(secondUser.age);
+secondUser.printDetailsUser();
+secondUser.printDetailsUserAccount();
--- src/classes.ts
+++ src/classes.ts
@@ -1,16 +1,38 @@
 class User {
-  name: string;
-  age: number;
+  private name: string;
+  readonly age: number;
 
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
 
-  printDetails(): void {
-    console.log(`Name: ${this.name} Age: ${this.age}`);
+  printDetailsUser(): void {
+    console.log(`Name: ${this.name} - Age: ${this.age}`);
+  }
+}
+
+class UserAccount extends User {
+  public nickname: string;
+  protected level: number;
+
+  constructor(name: string, age: number, nickname: string, level: number) {
+    super(name, age);
+    this.nickname = nickname;
+    this.level = level;
+  }
+
+  printDetailsUserAccount(): void {
+    console.log(`Nickname: ${this.nickname} - Age: ${this.age}  - Level: ${this.level}`);
   }
 }
 
 const firstUser = new User("Anderson", 27);
-firstUser.printDetails();
+firstUser.printDetailsUser();
+
+const secondUser = new UserAccount("Anderson", 27, "Alpin", 53);
+// secondUser.name = "Bruno"
+console.log(secondUser);
+console.log(secondUser.age);
+secondUser.printDetailsUser();
+secondUser.printDetailsUserAccount();
